887e18ad9be30d2590bcdee7e6ec0739885814ef
Morgan Sayers <morgansayers20298@gmail.com>
2023-04-17 02:53:59 -0500

    created experience, education, and projects pages

---
+++ Pages/Education.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace starzipper.github.io.Pages
+{
+    public class EducationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
